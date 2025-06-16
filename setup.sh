58c165f06d253864a71290447a893b0bd60eeaee
RonLang <ron.lan888@gmail.com>
2025-06-16 23:48:51 +0300

    added tmux stuff

--- setup.sh
+++ setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
-sudo apt install make gcc ripgrep unzip git xclip npm python3.10-venv neovim
+sudo apt install make gcc ripgrep unzip git xclip npm tmux python3.10-venv neovim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 # git clone https://github.com/Soyuz0/config_files.git "${XDG_CONFIG_HOME:-$HOME/.config}"
 # git init "${XDG_CONFIG_HOME:-$HOME/.config}"
 # cd "${XDG_CONFIG_HOME:-$HOME/.config}"
 # git remote add origin https://github.com/Soyuz0/config_files.git
 # git fetch
 # git checkout -t origin/main
-# brew install make gcc ripgrep unzip git xclip node python@3.10 neovim
+# brew install make gcc ripgrep unzip git xclip node tmux python@3.10 neovim
