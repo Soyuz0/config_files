a1e9ddd0f1081fb59f8f0824fa6b79f804cdbb89
Soyuz0 <ron.lan888@gmail.com>
2025-06-16 21:37:24 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,3 +3,8 @@ sudo add-apt-repository ppa:neovim-ppa/unstable -y
 sudo apt update
 sudo apt install make gcc ripgrep unzip git xclip npm python3.10-venv neovim
 # git clone https://github.com/Soyuz0/config_files.git "${XDG_CONFIG_HOME:-$HOME/.config}"
+# git init "${XDG_CONFIG_HOME:-$HOME/.config}"
+# cd "${XDG_CONFIG_HOME:-$HOME/.config}"
+# git remote add origin https://github.com/Soyuz0/config_files.git
+# git fetch
+# git checkout -t origin/main
