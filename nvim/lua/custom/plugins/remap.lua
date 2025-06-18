a7af11f665da199283692efeaeb501ac64e13b80
RonLang <ron.lan888@gmail.com>
2025-06-19 01:32:12 +0300

    small remapping

--- nvim/lua/custom/plugins/remap.lua
+++ nvim/lua/custom/plugins/remap.lua
@@ -53,4 +53,8 @@ end, {
 	silent = true,
 	desc = 'Run current Python file in new tmux pane (pause after exit)',
 })
+vim.keymap.set('n', '<C-d>', '<C-d>zz', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-u>', '<C-u>zz', { noremap = true, silent = true })
+vim.keymap.set('n', 'n', 'nzzzv', { noremap = true, silent = true })
+vim.keymap.set('n', 'N', 'Nzzzv', { noremap = true, silent = true })
 return {}
