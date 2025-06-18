0cca5775b8ce6d634478885ed81baf64f44df0db
RonLang <ron.lan888@gmail.com>
2025-06-18 18:17:08 +0300

    added global words

---
+++ nvim/lua/custom/plugins/global_copy.lua
@@ -0,0 +1,15 @@
+return {
+  'ojroques/nvim-osc52',
+  opts = {},
+  config = function()
+    require('osc52').setup {
+      max_length = 0,
+      trim = false,
+      silent = false,
+    }
+    local osc52 = require 'osc52'
+    vim.keymap.set('n', '<leader>cc', osc52.copy_operator, { expr = true, desc = 'OSC52 copy (operator/motion)' })
+
+    vim.keymap.set({ 'x', 'v' }, '<leader>cc', osc52.copy_visual, { desc = 'OSC52 copy (visual)' })
+  end,
+}
