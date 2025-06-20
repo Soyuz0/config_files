02c5622fab1cf353f3ea06cb85e1559adc0f5f02
RonLang <ron.lan888@gmail.com>
2025-06-20 16:56:50 +0300

    added better harpoon shorc

--- nvim/lua/custom/plugins/harpoon.lua
+++ nvim/lua/custom/plugins/harpoon.lua
@@ -1,21 +1,24 @@
 return {
-	'ThePrimeagen/harpoon',
-	config = function()
-		local mark = require 'harpoon.mark'
-		local ui = require 'harpoon.ui'
-		vim.keymap.set('n', '<leader>f', mark.add_file, { desc = 'Add file to harpoon' })
-		vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
-		-- vim.keymap.set('n', '<C-1>', function()
-		-- 	ui.nav_file(1)
-		-- end)
-		-- vim.keymap.set('n', '<C-2>', function()
-		-- 	ui.nav_file(2)
-		-- end)
-		-- vim.keymap.set('n', '<C-3>', function()
-		-- 	ui.nav_file(3)
-		-- end)
-		-- vim.keymap.set('n', '<C-4>', function()
-		-- 	ui.nav_file(4)
-		-- end)
-	end,
+  'ThePrimeagen/harpoon',
+  config = function()
+    local mark = require 'harpoon.mark'
+    local ui = require 'harpoon.ui'
+    vim.keymap.set('n', '<leader>f', mark.add_file, { desc = 'Add file to harpoon' })
+    vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
+    vim.keymap.set('n', '<C-H>', function()
+      ui.nav_file(1)
+    end)
+    vim.keymap.set('n', '<C-J>', function()
+      ui.nav_file(2)
+    end)
+    vim.keymap.set('n', '<C-K>', function()
+      ui.nav_file(3)
+    end)
+    vim.keymap.set('n', '<C-L>', function()
+      ui.nav_file(4)
+    end)
+    vim.keymap.set('n', '<C-:>', function()
+      ui.nav_file(5)
+    end)
+  end,
 }
